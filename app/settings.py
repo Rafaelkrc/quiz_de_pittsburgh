d8952f091ad768d22b5615c69c95085683cc3c4c
Rafaelkrc <rafaelkrc@hotmail.com>
2024-09-16 12:58:06 +0100

    personalizado o usuário padrão
    close #3

--- app/settings.py
+++ app/settings.py
@@ -28,10 +28,11 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-
+AUTH_USER_MODEL = 'base.User'
 # Application definition
 
 INSTALLED_APPS = [
+    'base',
     'quiz',
     'django.contrib.admin',
     'django.contrib.auth',
